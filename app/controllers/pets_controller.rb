c89e8d2dacb41c2c634f9a66db082c33ca9ebebd
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-08-31 12:31:43 -0700

    added CRUD endpoint handling

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,32 @@ class PetsController < ApplicationController
         pet = Pet.find(params[:id])
         render json: pet
     end
+    def create 
+        pet = Pet.create(pet_params)
+        render json: pet
+    end
+    def update
+        pet = Pet.find_by(id: params[:id])
+        if pet
+            pet.update(pet_params)
+            render json: pet
+        else
+            render json: { error: "pet not found"}, status: :not_found
+        end
+    end
+    def destroy
+        pet = Pet.find_by(id: params[:id])
+        if pet
+            pet.destroy
+            render json: {}
+        else
+            render json: { error: "pet not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def pet_params
+        params.permit(:breed, :name, :age, :weight, :gps_chip, :shelter_id)
+    end
 end
